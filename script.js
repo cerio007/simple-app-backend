83373ec599ffe15b3c3f42a063429b54539e6c36
Abosi Chijioke Cyprian <99417318+cerio007@users.noreply.github.com>
2025-02-01 19:27:59 +0100

    Updates script.js
    Add an env to secure my db

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const Product = require('./models/product.model.js');
@@ -8,22 +10,25 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.use('/api/products', productRoute);
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node API Server checking it out now, it seems it worked');
+  res.send(
+    'Hello from Node API Server checking it out now, it seems it worked'
+  );
 });
 
-mongoose.connect('mongodb+srv://abosicyprian07:GGkTVSPU9X4nflIm@backenddb.x0jjl.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
-.then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log('Connected to database!');
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
-})
-.catch(() => {
+  })
+  .catch(() => {
     console.log('Connection failed!');
-});
+  });
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log('Server is running on port 3000'));
